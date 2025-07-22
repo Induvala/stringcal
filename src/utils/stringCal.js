a0b91d215af117a4a2c17bf6933cf4bf13ea2348
Indu <induvala305@gmail.com>
2025-07-22 18:05:21 +0530

    calculate numbers

--- src/utils/stringCal.js
+++ src/utils/stringCal.js
@@ -1,3 +1,16 @@
 export function AddValue(numbers){
     if(!numbers) return 0;
+
+    let delimiter = /,|\n/;
+    let numberStr = numbers;
+
+    if(numbers.startsWith("//")){
+        const match = numbers.match(/^\/\/(.+)\n(.*)/);
+        if(match){
+            delimiter = new RegExp(match[1]);
+            numberStr = match[2];
+        }
+    }
+
+    
 }
